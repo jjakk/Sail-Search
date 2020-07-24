741c6bd6177c56fe44c580dd055ccb9de271146d
Jak <jaksawesome10@gmail.com>
2020-07-24 17:22:44 -0400

    e n h a n c e m e n t

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 
 function search(){
-  window.location = 'https://www.google.com/search?q=' + document.querySelector('#search').value;
+  window.location = 'https://www.google.com/search?q=' + document.getElementById('search').value;
 }
